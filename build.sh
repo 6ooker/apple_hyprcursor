69be9e173ec20473fd0b1df881e24f35fac2db27
6ooker <87530397+6ooker@users.noreply.github.com>
2026-05-01 23:23:19 +0200

    added help text, little optimization

--- build.sh
+++ build.sh
@@ -2,32 +2,48 @@
 
 version="0.1"
 
+function show_help() {
+  echo "Usage: build.sh [OPTIONS]"
+  echo "Options:"
+  echo "  -h  --help                        Show this help text"
+  echo "  -b  --background  <hex-color>     Set background color"
+  echo "  -f  --foreground  <hex-color>     Set foreground color"
+  echo "  -k  --keepbuild                   Don't delete generated build directory"
+  echo ""
+  echo "Example: ./build.sh -b '#000000' -f '#FFFFFF' -k"
+}
+
+if [ "$#" -eq 0 ]; then
+  show_help
+  exit 1
+fi
+
 while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
-  -V | --version )
+  -v | --version )
     echo "build script version: $version"
     exit
     ;;
-  -bg | --background )
-    shift; bg=$1
+  -h | --help )
+    show_help
+    exit
     ;;
-  -fg | --forground )
-    shift; fg=$1
+  -b | --background )
+    shift; bg="$1"
+    ;;
+  -f | --foreground )
+    shift; fg="$1"
     ;;
   -k | --keepbuild )
     keep=1
-    ;;
-  -f | --flag )
-    flag=1
     ;;
 esac; shift; done
 if [[ "$1" == '--' ]]; then shift; fi
 
 # TODO: Error handling / formatting for bg/fg options
-# TODO: Help
-# TODO: Verbose logs
+# TODO: Better logs
 
 set_color() {
-  sed -i -e "s/#00FF00/$2/g" -e "s/#0000FF/$3/g" $1/*.svg
+  sed -i -e "s/#00FF00/$2/g" -e "s/#0000FF/$3/g" "$1"/*.svg
 }
 
 # Make clean build dir
@@ -38,7 +54,7 @@ for entry in src/*; do
   cp -r "$entry" build/hyprcursors/
 done
 
-cd build
+cd build || exit
 
 cat << EOF > manifest.hl
 name = macOS-hypr
@@ -48,7 +64,7 @@ cursors_directory = hyprcursors
 EOF
 
 for file in hyprcursors/*; do
-  set_color $file "${fg:-#000000}" "${bg:-#FFFFFF}"
+  set_color "$file" "${fg:-#000000}" "${bg:-#FFFFFF}"
 done
 
 cd ..
@@ -65,7 +81,7 @@ if [ $? == 0 ]; then
   fi
 fi
 
-cd out
+cd out || exit
 
 mv theme_macOS-hypr macOS-hypr
 
